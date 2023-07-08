20abbe7799f34643e3d9bd10159f342b351040bf
Marcos <marcosviniciohd@hotmail.com>
2023-07-08 11:18:41 -0300

    Create project and folder diagrama-classes

---
+++ crud-prod-cat-user/src/main/java/br/edu/iftm/crud/prod/cat/user/CrudProdCatUserApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.crud.prod.cat.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudProdCatUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudProdCatUserApplication.class, args);
+    }
+
+}
---
+++ crud-prod-cat-user/src/test/java/br/edu/iftm/crud/prod/cat/user/CrudProdCatUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.crud.prod.cat.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProdCatUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
